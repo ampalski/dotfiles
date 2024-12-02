d47b4502e3b97a7923fab4247e4a2a28e5d9ad47
Andrew Palski <ampalski@gmail.com>
2024-12-02 01:13:44 -0700

    batman

---
+++ julia/.julia/config/startup.jl
@@ -0,0 +1,19 @@
+ENV["JULIA_PKG_USE_CLI_GIT"]=true
+
+import Pkg
+let
+    pkgs = ["Revise", "OhMyREPL", "LanguageServer"]
+    for pkg in pkgs
+        if Base.find_package(pkg) === nothing
+            Pkg.add(pkg)
+        end
+    end
+end
+
+using Revise
+using OhMyREPL
+@async begin
+    while !isdefined(Base, :active_repl) sleep(0.1) end
+    OhMyREPL.Prompt.insert_keybindings()
+end
+OhMyREPL.input_prompt!("λ ", :green)    	
